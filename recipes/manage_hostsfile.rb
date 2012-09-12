7648be72590fefcae8dfdeb5147c1da216d9cea9
Chris Roberts <chrisroberts.code@gmail.com>
2012-09-12 15:35:51 -0700

    Split hostname and aliases

--- recipes/manage_hostsfile.rb
+++ recipes/manage_hostsfile.rb
@@ -9,8 +9,10 @@ managed_hosts.merge!(managed_hosts_bag['maps']) if managed_hosts_bag
 managed_hosts.merge!(node[:dnsmasq][:managed_hosts].to_hash) if node[:dnsmasq][:managed_hosts]
 
 managed_hosts.each do |ip, host|
+  host = host.is_a?(Array) ? host : host.split(' ')
   hosts_file_entry ip do
-    hostname host
+    hostname host.shift
+    aliases host unless host.empty?
     comment 'dnsmasq managed entry'
   end
 end
