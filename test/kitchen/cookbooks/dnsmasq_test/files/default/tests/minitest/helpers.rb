c8f1c0de27d28f965079de508c98dc2c9aa57114
Chris Roberts <chrisroberts.code@gmail.com>
2012-09-13 11:05:58 -0700

    Add testing

---
+++ test/kitchen/cookbooks/dnsmasq_test/files/default/tests/minitest/helpers.rb
@@ -0,0 +1,7 @@
+require 'minitest/spec'
+
+module DnsMasqTestHelper
+  include MiniTest::Chef::Assertions
+  include MiniTest::Chef::Context
+  include MiniTest::Chef::Resources
+end
---
+++ test/kitchen/cookbooks/dnsmasq_test/recipes/default.rb
@@ -0,0 +1,7 @@
+node[:dnsmasq][:enable_dns] = true
+node[:dnsmasq][:managed_hosts] = {
+  '192.168.0.2' => 'google.com www.google.com mail.google.com',
+  '192.168.0.3' => 'yahoo.com'
+}
+
+include_recipe 'dnsmasq'
