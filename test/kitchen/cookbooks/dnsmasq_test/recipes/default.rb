b633950e2cd2dd9509204ed6b4a500d5f213309a
Arnold Krille <arnold@arnoldarts.de>
2013-06-30 00:38:07 +0200

    Fix for changes in newer chef-versions.

--- test/kitchen/cookbooks/dnsmasq_test/recipes/default.rb
+++ test/kitchen/cookbooks/dnsmasq_test/recipes/default.rb
@@ -1,5 +1,5 @@
-node[:dnsmasq][:enable_dns] = true
-node[:dnsmasq][:managed_hosts] = {
+node.set[:dnsmasq][:enable_dns] = true
+node.set[:dnsmasq][:managed_hosts] = {
   '192.168.0.2' => 'google.com www.google.com mail.google.com',
   '192.168.0.3' => 'yahoo.com'
 }
