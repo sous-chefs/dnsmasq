b2343b9865d3c6e774b6935d96d52a511f7126e5
Matt Ray <matthewhray@gmail.com>
2013-10-03 23:16:41 -0500

    update supported OSs

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
 
 depends 'hosts_file'
+
+%w{ubuntu debian redhat centos scientific oracle}.each do |os|
+  supports os
+end
