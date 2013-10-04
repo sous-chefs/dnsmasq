e8597b227e0e924fc7f69f8966a459c36e7f5d90
Matt Ray <matthewhray@gmail.com>
2013-10-03 19:17:43 -0500

    updated tests to work with Berkshelf and more platforms

--- test/cookbooks/dnsmasq_test/recipes/default.rb
+++ test/cookbooks/dnsmasq_test/recipes/default.rb
@@ -1,16 +1,9 @@
-node.default[:hosts_file][:path] = '/tmp/hosts'
 node.default[:dnsmasq][:enable_dns] = true
-node.default[:dnsmasq][:managed_hosts] = {
-  '192.168.0.2' => 'google.com www.google.com mail.google.com',
-  '192.168.0.3' => 'yahoo.com'
-}
-
 node.default[:dnsmasq][:enable_dhcp] = true
+
 node.default[:dnsmasq][:dhcp] = {
   'enable-tftp' => nil,
-  'interface' => 'eth1',
-  'domain' => 'yourdomain.com',
-  'tftp-root' => '/var/lib/tftpboot'
+  'interface' => 'eth1'
 }
 
 include_recipe 'dnsmasq'
