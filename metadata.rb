57fbe3e3bb954e5d6121eefedd45c6e19020a51e
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-23 11:31:26 -0800

    Update version for development v0.2.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,8 @@ maintainer 'Chris Roberts'
 maintainer_email 'chrisroberts.code@gmail.com'
 license 'Apache 2.0'
 description 'Installs and configures dnsmasq'
-version '0.2.0'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.2.1'
 
 depends 'hosts_file'
 
