5130f50ece9efe739b867692b58557c0ab5db541
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-23 10:42:01 -0800

    Use `#map` directly to get contents returned

--- recipes/manage_hostsfile.rb
+++ recipes/manage_hostsfile.rb
@@ -9,7 +9,7 @@ managed_hosts.merge!(managed_hosts_bag['maps']) if managed_hosts_bag
 managed_hosts.merge!(node[:dnsmasq][:managed_hosts].to_hash) if node[:dnsmasq][:managed_hosts]
 
 managed_hosts.each do |ip, host|
-  host = host.is_a?(Array) ? host.each.collect {|x|x} : host.split(' ')
+  host = host.is_a?(Array) ? host.map{|i|i} : host.split(' ')
   hosts_file_entry ip do
     hostname host.shift
     aliases host unless host.empty?
