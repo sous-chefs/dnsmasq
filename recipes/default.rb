6e7977b70b672f0fe03de6a47e34e606ec0f8e5c
Denis Barishev <denis.barishev@gmail.com>
2014-04-30 15:00:22 +0700

    fixed service start order

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,13 @@
 package 'dnsmasq'
 
-service 'dnsmasq' do
-  action [:enable, :start]
-end
-
 if(node[:dnsmasq][:enable_dns])
   include_recipe 'dnsmasq::dns'
 end
 
 if(node[:dnsmasq][:enable_dhcp])
   include_recipe 'dnsmasq::dhcp'
+end
+
+service 'dnsmasq' do
+  action [:enable, :start]
 end
