fb9449318f1be787c6240a6b26d82220adcde995
Xorima <4923914+Xorima@users.noreply.github.com>
2018-10-18 13:01:53 +0100

    Change Maintainer to Sous

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name              'dnsmasq'
-maintainer        'Chris Roberts'
-maintainer_email  'chrisroberts.code@gmail.com'
+maintainer        'Sous-Chefs'
+maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures dnsmasq'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-chef_version      '>= 13.0' if respond_to?(:chef_version)
+chef_version      '>= 13.0'
 source_url        'https://github.com/sous-chefs/dnsmasq'
 issues_url        'https://github.com/sous-chefs/dnsmasq/issues'
 version           '0.2.0'
