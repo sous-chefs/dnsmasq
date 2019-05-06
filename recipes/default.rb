415f966c34624e1686f52a2b705ef85b078672cc
Dan Webb <dan.webb@damacus.io>
2019-05-06 23:48:01 +0100

    Add dokken testing (#32)
    * Add dokken testing
    
    * Fix delivery warnings
    
    * Fix markdown
    
    * Fix yaml
    
    * Provide a default stubbed action for the resources
    
    * Add platofrm centos-6
    
    * Make sure the service doesn't conflict on Ubuntu 18.04

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,11 @@
+if platform?('ubuntu') && node['platform_version'] >= '18.04'
+  service 'systemd-resolved' do
+    action :stop
+  end
+end
+
 package 'dnsmasq'
+user 'dnsmasq'
 
 include_recipe 'dnsmasq::dns' if node['dnsmasq']['enable_dns']
 include_recipe 'dnsmasq::dhcp' if node['dnsmasq']['enable_dhcp']
---
+++ resources/config.rb
@@ -0,0 +1,2 @@
+action :create do
+end
