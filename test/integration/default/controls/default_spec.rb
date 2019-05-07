c2635f87e1b31c8bf3fd1cc6568b02e772f68ba6
Dan Webb <dan.webb@damacus.io>
2019-05-07 09:15:45 +0100

    Switch from serverspec to inspec

--- test/integration/default/controls/default_spec.rb
+++ test/integration/default/controls/default_spec.rb
@@ -1,11 +1,11 @@
-require 'spec_helper'
-
-describe 'dnsmasq::default' do
+control 'service' do
   describe service('dnsmasq') do
     it { should be_enabled }
     it { should be_running }
   end
+end
 
+control 'configuration' do
   describe file('/etc/dnsmasq.d/dhcp.conf') do
     it { should be_mode '644' }
     its(:content) { should match(/^dhcp-host=01:23:ab:cd:01:02,larry,10\.0\.0\.10$/) }
@@ -22,22 +22,28 @@ describe 'dnsmasq::default' do
     its(:content) { should match(/^no-resolv$/) }
     its(:content) { should match(/^server=8\.8\.8\.8$/) }
   end
+end
 
+control 'dns resolution' do
   describe command('host google.com') do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should match(/^google.com has address/) }
   end
+end
 
+control 'dns port' do
   describe port(53) do
     it { should be_listening }
   end
+end
 
-  # bootp/dhcp
+control 'bootp/dhcp' do
   describe port(67) do
     it { should be_listening }
   end
+end
 
-  # tftp
+control 'tftp' do
   describe port(69) do
     it { should be_listening }
   end
