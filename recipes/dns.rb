aa5e0dd76b08720c363249c1000657d318ec1e97
Dan Webb <dan.webb@damacus.io>
2019-08-19 20:40:17 +0100

    Auto correct using cookstyle

--- recipes/dns.rb
+++ recipes/dns.rb
@@ -6,7 +6,7 @@ dns_config['no-dhcp-interface='] = nil unless node['dnsmasq']['enable_dhcp']
 
 template '/etc/dnsmasq.d/dns.conf' do
   source 'dynamic_config.erb'
-  mode 0644
+  mode '644'
   variables(
     config: dns_config,
     list: node['dnsmasq']['dns_options']
